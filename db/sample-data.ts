6e08d975561a0ceed370f87b6b092e89a82b7333
JSharles <jeancharlesbarq@gmail.com>
2025-01-26 22:18:07 +0100

    fix: deployment

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -5,14 +5,14 @@ const sampleData = {
     {
       name: "Julien Ribeiro",
       email: "amedutambour@gmail.com",
-      password: hashSync("123456", 10),
+      password: hashSync("", 10),
       role: "admin",
     },
     {
       name: "Jean-Charles Barq",
       email: "jeancharlesbarq@gmail.com",
-      password: hashSync("123456", 10),
-      role: "user",
+      password: hashSync("", 10),
+      role: "admin",
     },
   ],
   products: [
